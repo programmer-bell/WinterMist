f804f45921af513474dd0d10e1ae84355e9f7784
Badhan Sarkar <badhansarkar164@gmail.com>
2025-11-26 09:22:55 +0530

    Adding app in root project

--- py_client/base.py
+++ py_client/base.py
@@ -1,7 +1,9 @@
 import requests
 
-endpoint = 'https://httpbin.org/status/200'
-endpoint = 'https://httpbin.org/'
+# endpoint = 'https://httpbin.org/status/200'
+# endpoint = 'https://httpbin.org/'
+endpoint = 'http://127.0.0.1:8000/'
 
 get_respone = requests.get(endpoint) # Basic API Request
 print(get_respone.text) # Print raw text response 
+
